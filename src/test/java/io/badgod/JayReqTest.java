fd63e56aba3c1006497f76676cdb88e3451b864a
Robert Pelger <r.pelger@reply.de>
2024-01-24 21:15:25 +0100

    Added exceptional testcase and cleaned up tests.

--- src/test/java/io/badgod/JayReqTest.java
+++ src/test/java/io/badgod/JayReqTest.java
@@ -2,35 +2,35 @@ package io.badgod;
 
 import io.badgod.jayreq.JayReq;
 import io.badgod.jayreq.Request;
+import io.badgod.jayreq.http.HttpError;
 import io.badgod.jayreq.http.JayReqHttpClient;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class JayReqTest {
 
+    private final static String GH_URL = "https://api.github.com/repos/robpelger/jayreq";
+    public static final String EXPECTED_DESCRIPTION = "JayReq is a batteries-included HTTP client for Java.";
+
     @Test
     void should_do_get_request_via_shortcut() {
-        var response = JayReq.get(
-                "https://api.github.com/repos/robpelger/jayreq",
-                GithubRepoResponse.class);
-
-        assertThat(
-                response.body().description(),
-                is("JayReq is a batteries-included HTTP client for Java."));
+        var resp = JayReq.get(GH_URL, GithubRepoResponse.class);
+        assertThat(resp.body().description(), is(EXPECTED_DESCRIPTION));
     }
 
     @Test
     void should_do_get_request_via_instance() {
-        JayReq jayreq = new JayReqHttpClient();
-        var response = jayreq.get(
-            new Request("https://api.github.com/repos/robpelger/jayreq"),
-            GithubRepoResponse.class);
+        JayReq jr = new JayReqHttpClient();
+        var resp = jr.get(new Request(GH_URL), GithubRepoResponse.class);
+        assertThat(resp.body().description(), is(EXPECTED_DESCRIPTION));
+    }
 
-        assertThat(
-            response.body().description(),
-            is("JayReq is a batteries-included HTTP client for Java."));
+    @Test
+    void should_throw_on_connection_error() {
+        assertThrows(HttpError.class, () -> JayReq.get("http://does-not.exist", String.class));
     }
 
     private record GithubRepoResponse(String description) {
