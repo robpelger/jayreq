0e36bb942505a4358ea9e8c7f171616b0d6d4004
Robert Pelger <r.pelger@reply.de>
2024-02-21 08:46:28 +0100

    Added simple (naive) SSL helper

---
+++ src/main/java/io/badgod/jayreq/ssl/SSLContextFactory.java
@@ -0,0 +1,25 @@
+package io.badgod.jayreq.ssl;
+
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+
+import javax.net.ssl.SSLContext;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+
+public class SSLContextFactory {
+
+    public static SSLContext create(String keystoreFile, String keystorePassword) {
+        try (InputStream in = new FileInputStream(keystoreFile)) {
+            KeyStore keystore = KeyStore.getInstance("PKCS12");
+            keystore.load(in, keystorePassword.toCharArray());
+            return SSLContextBuilder.create()
+                .loadKeyMaterial(keystore, keystorePassword.toCharArray())
+                .loadTrustMaterial(keystore, (chain, authType) -> true)
+                .build();
+        } catch (Exception e) {
+            throw new RuntimeException("Error initialising SSL-Context", e);
+        }
+    }
+
+}
